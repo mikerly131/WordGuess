233818e95eab1732c44225e0decba079e4d72271
Mike <mike@ZipcodersMBP83.fios-router.home>
2023-11-02 00:04:15 -0400

    basic structure is in, methods and control left

--- src/main/java/com/github/zipcodewilmington/Wordguess.java
+++ src/main/java/com/github/zipcodewilmington/Wordguess.java
@@ -9,8 +9,24 @@ package com.github.zipcodewilmington;
 public class Wordguess {
     private String[] words = {"banana", "aardvark", "turtle", "picture", "gravy" };
 
+    // Other instance variables?...i suspect something horribly wrong is about to happen
+    private int guessesLeft = 0;
+    private String secretWord = "";
+
+    private char[] secretWordArr;
+    private String guessArrStr = "";  // array? why no string friend? my brain tired
+
+    private char[] guessArr;
+
+    // But should the arrays be here?
+
     public Wordguess() {
         this.words = words;
+        this.secretWord = secretWord;
+        this.guessArrStr = guessArrStr;
+        this.guessesLeft = guessesLeft;
+        this.secretWordArr = secretWordArr;
+        this.guessArr = guessArr;
 
     }
 
@@ -23,19 +39,42 @@ public class Wordguess {
         printGameStart();
 
         while (wantToPlay) {
+
+            // Start a fresh game: wordGuessed is false now
             Boolean wordGuessed = false;
 
+            // Get a new secret word
+            this.secretWord = getRandomWord();
+
+            // Setup an array of secret word characters to compare to guessed characters
+            this.secretWordArr = getSecretWordArr(this.secretWord);
+
+            // Setup an array of guess characters equal to length of secret word
+            this.guessArr = getGuessArrInit(this.secretWordArr);
+
+            this.guessArrStr = this.guessArr.toString();
+
             while (!wordGuessed) {
-                String secretWord = getRandomWord();
-                char[] secretWordArray = getSecretWordArr(secretWord);
+
+                // Where and How to do these things...
+                // Print guesses left
+                // print out guessArray
+                // declare guesses left = guess or secret word or secret word array length;
+                // Prompt player to enter a character {restrict input to char}
+                // check if character in secret word array
+                // everywhere it appears replace _ with character in guess array
+                // check if all characters guessed
+                // check if any guesses left
+                // update guesses left
 
 
                 wordGuessed = true;
 
             }
-
+            // prompt to play again
             wantToPlay = false;
         }
+        // quit
         printGameOver();
     }
 
@@ -59,12 +98,17 @@ public class Wordguess {
         return charRandomWord;
     }
 
-    public String getSecretWordObfuscated(char[] secretWordToPrint) {
+    public char[] getGuessArrInit(char[] secretWordToPrint) {
         StringBuilder replaceCharacters = new StringBuilder();
         for(int i = 0; i < secretWordToPrint.length; i++) {
-            replaceCharacters.append("_ ");
+            replaceCharacters.append("_");
+            if (i < secretWordToPrint.length - 1) {
+                replaceCharacters.append(" ");
+            }
         }
-        return replaceCharacters.toString();
+        String swap = replaceCharacters.toString();
+        char[] replaceCharactersArr = swap.toCharArray();
+        return replaceCharactersArr;
 
     }
 
