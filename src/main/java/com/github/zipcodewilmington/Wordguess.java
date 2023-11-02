449f3071eb5d90f2322d9450df62cd45ea709db3
Mike <mike@ZipcodersMBP83.fios-router.home>
2023-11-01 22:49:58 -0400

    more methods for secret words, chars

--- src/main/java/com/github/zipcodewilmington/Wordguess.java
+++ src/main/java/com/github/zipcodewilmington/Wordguess.java
@@ -20,11 +20,23 @@ public class Wordguess {
     }
     public void runGame() {
         Boolean wantToPlay = true;
+        printGameStart();
+
         while (wantToPlay) {
-            String secretWord = getRandomWord();
-            System.out.print(secretWord);
+            Boolean wordGuessed = false;
+
+            while (!wordGuessed) {
+                String secretWord = getRandomWord();
+                char[] secretWordArray = getSecretWordArr(secretWord);
+
+
+                wordGuessed = true;
+
+            }
+
             wantToPlay = false;
         }
+        printGameOver();
     }
 
     // Get a random number anywhere from 0 to length of words array.
@@ -39,6 +51,31 @@ public class Wordguess {
     public String getRandomWord() {
         String randomWord = words[getRandomNumber(words.length)];
         return randomWord;
+    }
+
+    //Get a character array of the randomWord
+    public char[] getSecretWordArr(String theRandomWord) {
+        char[] charRandomWord = theRandomWord.toCharArray();
+        return charRandomWord;
+    }
+
+    public String getSecretWordObfuscated(char[] secretWordToPrint) {
+        StringBuilder replaceCharacters = new StringBuilder();
+        for(int i = 0; i < secretWordToPrint.length; i++) {
+            replaceCharacters.append("_ ");
+        }
+        return replaceCharacters.toString();
+
+    }
+
+    // Say something when the game starts
+    public String printGameStart() {
+        return "Welcome to Thunderdome!! \nTime to guess the word or be stuck in bad spandex forever. \n";
+    }
+
+    // Say something when the game is over
+    public String printGameOver() {
+        return "You have ended the game. Good bye.";
     }
 
 
--- src/test/java/com/github/zipcodewilmington/WordguessTest.java
+++ src/test/java/com/github/zipcodewilmington/WordguessTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
  * @version 1.0.0
  * @date 5/27/21 11:02 AM
  */
-public class HangmanTest {
+public class WordguessTest {
 
     @Test
     public void getRandomNumber() {
