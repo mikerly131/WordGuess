cd137cc232ca3861c9c7ced24a3f2f4234f6f7ed
Mike <mike@ZipcodersMBP83.fios-router.home>
2023-11-02 22:10:08 -0400

    added ifs and for loopsmore to do

--- src/main/java/com/github/zipcodewilmington/Wordguess.java
+++ src/main/java/com/github/zipcodewilmington/Wordguess.java
@@ -1,8 +1,10 @@
 package com.github.zipcodewilmington;
 
 
+import java.util.Scanner;
+
 /**
- * @author mikerly131
+ * @author mikerly131  (are we supposed to put our names here?)
  * @version 1.0.0
  * @date 11/2/23 11:30 PM
  */
@@ -36,7 +38,7 @@ public class Wordguess {
     }
     public void runGame() {
         Boolean wantToPlay = true;
-        printGameStart();
+        System.out.print(printGameStart());
 
         while (wantToPlay) {
 
@@ -47,22 +49,33 @@ public class Wordguess {
             this.secretWord = getRandomWord();
 
             // Setup an array of secret word characters to compare to guessed characters
-            this.secretWordArr = getSecretWordArr(this.secretWord);
+            this.secretWordArr = getSecretWordArr(secretWord);
+
+            // Set guesses left = length of secret word.  A rule in our game
+            this.guessesLeft = setGuessesLeft(secretWord);
 
             // Setup an array of guess characters equal to length of secret word
-            this.guessArr = getGuessArrInit(this.secretWordArr);
+            this.guessArr = getGuessArrInit(secretWordArr);
 
-            this.guessArrStr = this.guessArr.toString();
+            // Setup a string fo reasy printing out of the array
+            this.guessArrStr = getGuessArrStr(guessArr);
 
             while (!wordGuessed) {
 
                 // Where and How to do these things...
+                //System.out.print(this.secretWord);
+                //System.out.print(this.secretWordArr);
                 // Print guesses left
+                System.out.println(getGuessesLeftMessage());
+
                 // print out guessArray
-                // declare guesses left = guess or secret word or secret word array length;
+                System.out.println(this.guessArrStr);
+
                 // Prompt player to enter a character {restrict input to char}
-                // check if character in secret word array
-                // everywhere it appears replace _ with character in guess array
+                char userGuess = getUserGuess();
+                this.guessArr = getGuessArrUpdate(secretWordArr, userGuess);
+
+                // update guesses left = guess or secret word or secret word array length;
                 // check if all characters guessed
                 // check if any guesses left
                 // update guesses left
@@ -75,7 +88,7 @@ public class Wordguess {
             wantToPlay = false;
         }
         // quit
-        printGameOver();
+        System.out.print(printGameOver());
     }
 
     // Get a random number anywhere from 0 to length of words array.
@@ -98,6 +111,34 @@ public class Wordguess {
         return charRandomWord;
     }
 
+    // Set the guesses left
+    public int setGuessesLeft (String secretWord) {
+        int guessLeft = secretWord.length();
+        return guessLeft;
+    }
+
+    // Get the guesses left
+    public int getGuessesLeft () {
+        return guessesLeft;
+    }
+
+    // Get the next remaining guesses to print string
+    public String getGuessesLeftMessage() {
+        return ("You have " + getGuessesLeft() + " guesses left.");
+    }
+
+    public char getUserGuess() {
+        Scanner sc = new Scanner(System.in);
+        String userGuess = sc.nextLine();
+        char charGuess = userGuess.charAt(0);
+        return charGuess;
+    }
+
+
+
+
+    // Get the initial _ replaced letters and space added guess array
+    // Refactor:  there has got to be a better way to do this, i will learn when i have time
     public char[] getGuessArrInit(char[] secretWordToPrint) {
         StringBuilder replaceCharacters = new StringBuilder();
         for(int i = 0; i < secretWordToPrint.length; i++) {
@@ -112,9 +153,47 @@ public class Wordguess {
 
     }
 
+    // Check if the users guessed character is in the secret word
+
+
+
+    // update the guessArr
+    public char[] getGuessArrUpdate(char[] secretWordToPrint, char userGuess) {
+
+        StringBuilder replaceCharacters = new StringBuilder();
+
+        // loop through secret word characters, see if letter in word.
+        for (int i = 0; i < secretWordToPrint.length; i++) {
+            //
+            if (userGuess == secretWordToPrint[i]) {
+                replaceCharacters.append(userGuess);
+                if (i < secretWordToPrint.length - 1) {
+                    replaceCharacters.append(" ");
+                }
+            } else {
+                replaceCharacters.append("_");
+                if (i < secretWordToPrint.length - 1) {
+                    replaceCharacters.append(" ");
+                }
+
+            }
+
+        }
+        String swap = replaceCharacters.toString();
+        char[] replaceCharactersArr = swap.toCharArray();
+        return replaceCharactersArr;
+    }
+
+
+    //
+    public String getGuessArrStr(char[] charArray) {
+        String guessString = String.valueOf(charArray);
+        return guessString;
+    }
+
     // Say something when the game starts
     public String printGameStart() {
-        return "Welcome to Thunderdome!! \nTime to guess the word or be stuck in bad spandex forever. \n";
+        return "Welcome to Thunderdome!! \nTime to guess the word or be stuck in bad spandex forever. \nGuess one letter at a time! \n";
     }
 
     // Say something when the game is over
